dc1ba10c299f4cf175be4c90dbd479f2b7eaeb86
Stephen A. Davis <23553256+webdavis@users.noreply.github.com>
2025-10-09 02:22:27 -0600

    Ensure `flake.nix` fie is available prior to linting

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -11,17 +11,23 @@ FLAKE_NIX_FILE="flake.nix"
 PROJECT_ROOT=$(git rev-parse --show-toplevel 2>/dev/null)
 cd "$PROJECT_ROOT"
 
-if [ ! -f "$BREWFILE" ]; then
-  echo "Error: '$BREWFILE' is missing in the project root ($PROJECT_ROOT). Linting aborted." >&2
-  exit 1
-fi
+check_file() {
+  local file="$1"
 
+  if [ ! -f "$file" ]; then
+    echo "Error: '$file' is missing in the project root ($PROJECT_ROOT). Linting aborted." >&2
+    exit 1
+  fi
+}
+
+check_file "$BREWFILE"
 echo "RuboCop path: $(bundle exec which rubocop)"
 echo "RuboCop version: $(bundle exec rubocop -v)"
 echo "Linting '${BREWFILE}' with RuboCop..."
 bundle exec rubocop --display-time -- "$BREWFILE"
 echo
 
+check_file "$FLAKE_NIX_FILE"
 echo "nix fmt path: $(which treefmt)"
 echo "nix fmt version: $(nix fmt -- --version)"
 echo "Linting '${FLAKE_NIX_FILE}' with nix fmt..."
