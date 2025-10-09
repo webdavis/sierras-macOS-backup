77fd2bc30ba570eb21f14b2231ad09062c53dbb7
Stephen A. Davis <23553256+webdavis@users.noreply.github.com>
2025-10-08 22:52:22 -0600

    Add support for Nix expression formatting by configuring `nix fmt` to use `nixfmt-tree`
    See: https://github.com/NixOS/nixfmt?tab=readme-ov-file#nix-fmt-experimental

--- flake.nix
+++ flake.nix
@@ -6,49 +6,63 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" ] (system: let
-    pkgs = import nixpkgs { inherit system; };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" ] (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
 
-    baseShell = pkgs.mkShell {
-      buildInputs = [
-        pkgs.ruby_3_4  # This comes with Bundler included.
-      ];
+        nixfmt = nixpkgs.legacyPackages.${system}.nixfmt-tree;
 
-      # Note: This project already tracks the ./bundle/config file, which the ensures that gems
-      # are installed into the project-local './vendor/bundle' directory, but we enforce it
-      # here as a fail-safe.
-      shellHook = ''
-        echo
-        bundle config set path 'vendor/bundle'
+        baseShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.ruby_3_4 # This comes with Bundler included.
+            nixfmt
+          ];
 
-        if [ ! -d vendor/bundle ] || ! bundle check > /dev/null 2>&1; then
-          echo "Installing gems..."
-          bundle install --jobs 4 --retry 3
-          echo
-        fi
-      '';
-    };
+          # Note: This project already tracks the ./bundle/config file, which the ensures that gems
+          # are installed into the project-local './vendor/bundle' directory, but we enforce it
+          # here as a fail-safe.
+          shellHook = ''
+            echo
+            bundle config set path 'vendor/bundle'
 
-    interactiveShell = pkgs.mkShell {
-      buildInputs = baseShell.buildInputs;
-      shellHook = baseShell.shellHook + ''
-        red="\e[91m"
-        green="\e[32m"
-        blue="\e[34m"
-        bold="\e[1m"
-        reset="\e[0m"
+            if [ ! -d vendor/bundle ] || ! bundle check > /dev/null 2>&1; then
+              echo "Installing gems..."
+              bundle install --jobs 4 --retry 3
+              echo
+            fi
+          '';
+        };
 
-        projectName="$(basename "$PWD")"
+        interactiveShell = pkgs.mkShell {
+          buildInputs = baseShell.buildInputs;
+          shellHook = baseShell.shellHook + ''
+            red="\e[91m"
+            green="\e[32m"
+            blue="\e[34m"
+            bold="\e[1m"
+            reset="\e[0m"
 
-        echo -e "''${blue}Entering Brewfile linting environment...''${reset}\n"
-        echo -e "''${bold}Project:''${reset} ''${green}''${projectName}''${reset}"
-        echo -e "''${bold}Ruby version:''${reset} ''${red}${pkgs.ruby_3_4.version}''${reset}"
-        echo -e "''${bold}Rubocop version:''${reset} ''${red}$(bundle exec rubocop -v)''${reset}"
-      '';
-    };
-  in {
-      devShells.default = interactiveShell;
-      devShells.adhoc = baseShell;
-    }
-  );
+            projectName="$(basename "$PWD")"
+
+            echo -e "''${blue}Entering Brewfile linting environment...''${reset}\n"
+            echo -e "''${bold}Project:''${reset} ''${green}''${projectName}''${reset}"
+            echo -e "''${bold}Ruby version:''${reset} ''${red}${pkgs.ruby_3_4.version}''${reset}"
+            echo -e "''${bold}Rubocop version:''${reset} ''${red}$(bundle exec rubocop -v)''${reset}"
+          '';
+        };
+      in
+      {
+        devShells.default = interactiveShell;
+        devShells.adhoc = baseShell;
+        formatter = nixfmt;
+      }
+    );
 }
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,15 +3,16 @@
 # Exit immediately if any command fails, or if any variables are unset.
 set -eu
 
-# The target file.
-TARGET_FILE="dot_Brewfile"
+# Target files:
+BREWFILE="dot_Brewfile"
+FLAKE_NIX_FILE="flake.nix"
 
 # Ensure this script runs from the project root.
 PROJECT_ROOT=$(git rev-parse --show-toplevel 2>/dev/null)
 cd "$PROJECT_ROOT"
 
-if [ ! -f "$TARGET_FILE" ]; then
-  echo "Error: '$TARGET_FILE' is missing in the project root ($PROJECT_ROOT). Linting aborted." >&2
+if [ ! -f "$BREWFILE" ]; then
+  echo "Error: '$BREWFILE' is missing in the project root ($PROJECT_ROOT). Linting aborted." >&2
   exit 1
 fi
 
@@ -19,5 +20,8 @@ echo "RuboCop path: $(bundle exec which rubocop)"
 echo "RuboCop version: $(bundle exec rubocop -v)"
 echo
 
-echo "Linting '${TARGET_FILE}' with RuboCop..."
-bundle exec rubocop --display-time -- "$TARGET_FILE"
+echo "Linting '${BREWFILE}' with RuboCop..."
+bundle exec rubocop --display-time -- "$BREWFILE"
+
+echo "Linting '${FLAKE_NIX_FILE}' with nix fmt..."
+nix fmt "$FLAKE_NIX_FILE"
